81694e9f09d020d9a161e45e0066ea795f39fab3
ewel1991 <ewelina.beben.programista@gmail.com>
2024-10-19 15:26:58 +0200

    Dodanie zmiennych

--- main.cpp
+++ main.cpp
@@ -12,6 +12,14 @@ using namespace std;
 
 int main()
 {
+    vector <Adresat> adresaci;
+    vector <Uzytkownik> uzytkownicy;
+    Adresat adresat;
+    Uzytkownik uzytkownik;
+    char wybor;
+    int numerKolejnegoId = 0, iloscUzytkownikow = 0, idDoZmiany = 0, idZalogowanegoUzytkownika = 0;
+    string liniaDoZmiany ="";
+
 
     return 0;
 }
